f7ce825fbdac5ca120cfa6c247254621e53f69a8
WindomZ <git.windomz@gmail.com>
2017-08-10 16:12:02 +0800

    feat: add go-commander cli

--- main.go
+++ main.go
@@ -1,14 +1,42 @@
 package main
 
-import "github.com/WindomZ/lagou-jobs/lagou"
+import (
+	"fmt"
+
+	"github.com/WindomZ/go-commander"
+	"github.com/WindomZ/go-develop-kit/path"
+	"github.com/WindomZ/lagou-jobs/lagou"
+)
 
 func main() {
-	spider, err := lagou.New("./tests/asset/config.json")
-	if err != nil {
-		panic(err)
-	}
+	// lagou-jobs
+	commander.Program.
+		Version("v1.0.0")
 
-	if err := spider.Start(); err != nil {
-		panic(err)
-	}
+	// lagou-jobs <config-file>
+	commander.Program.
+		Command("<config-file>", "load the configuration file.").
+		Description("add user configuration").
+		Action(func(c commander.Context) error {
+			configPath := c.MustString("<config-file>")
+			if configPath == "." {
+				configPath = "config.json" // default filename
+			}
+
+			if ok, err := path.IsExist(configPath); err != nil {
+				return err
+			} else if !ok {
+				return fmt.Errorf("no \"%v\" file found", configPath)
+			}
+
+			spider, err := lagou.New(configPath)
+			if err != nil {
+				return err
+			}
+
+			return spider.Start()
+		})
+
+	commander.Program.Parse()
+
 }
